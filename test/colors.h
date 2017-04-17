41a473cfe514d4182f76b76885aa2e0d6f8b25e2
Chuck McManis <cmcmanis@mcmanis.com>
2017-04-16 20:05:49 -0700

    Mostly working, updated tests directory
    This commit gets most of V2 in place, it adds tests for most of
    the functions but they aren't well organized yet.

---
+++ test/colors.h
@@ -0,0 +1,48 @@
+/*
+ * colors.h
+ *
+ * Defines some letters as 'colors' for the test
+ * programs to use.
+ */
+#pragma once
+#include <gfx.h>
+
+GFX_COLOR letters[] = {
+	(GFX_COLOR) { .raw=(uint32_t)'0'},	/* 0 */
+	(GFX_COLOR) { .raw=(uint32_t)'1'},	/* 1 */
+	(GFX_COLOR) { .raw=(uint32_t)'2'},	/* 2 */
+	(GFX_COLOR) { .raw=(uint32_t)'3'},	/* 3 */
+	(GFX_COLOR) { .raw=(uint32_t)'4'},	/* 4 */
+	(GFX_COLOR) { .raw=(uint32_t)'5'},	/* 5 */
+	(GFX_COLOR) { .raw=(uint32_t)'6'},	/* 6 */
+	(GFX_COLOR) { .raw=(uint32_t)'7'},	/* 7 */
+	(GFX_COLOR) { .raw=(uint32_t)'8'},	/* 8 */
+	(GFX_COLOR) { .raw=(uint32_t)'9'},	/* 9 */
+	(GFX_COLOR) { .raw=(uint32_t)'A'},	/* 10 */
+	(GFX_COLOR) { .raw=(uint32_t)'B'},	/* 11 */
+	(GFX_COLOR) { .raw=(uint32_t)'C'},	/* 12 */
+	(GFX_COLOR) { .raw=(uint32_t)'D'},	/* 13 */
+	(GFX_COLOR) { .raw=(uint32_t)'E'},	/* 14 */
+	(GFX_COLOR) { .raw=(uint32_t)'F'},	/* 15 */
+	(GFX_COLOR) { .raw=(uint32_t)' '},	/* 16 */
+	(GFX_COLOR) { .raw=(uint32_t)'*'},	/* 17 */
+	(GFX_COLOR) { .raw=(uint32_t)'@'},	/* 18 */
+	(GFX_COLOR) { .raw=(uint32_t)'o'},	/* 19 */
+	(GFX_COLOR) { .raw=(uint32_t)'.'},	/* 20 */
+	(GFX_COLOR) { .raw=(uint32_t)'+'},	/* 21 */
+	(GFX_COLOR) { .raw=(uint32_t)'#'},	/* 22 */
+	(GFX_COLOR) { .raw=(uint32_t)'x'},	/* 23 */
+	(GFX_COLOR) { .raw=(uint32_t)'-'}	/* 24 */
+};
+
+#define C_ONE	letters[1]
+#define C_TWO	letters[1]
+#define C_THREE	letters[1]
+#define C_BLANK	letters[16]
+#define C_STAR	letters[17]
+#define C_PLUS	letters[21]
+#define C_HASH	letters[22]
+#define C_EX	letters[23]
+#define C_DASH	letters[24]
+#define C_AT	letters[18]
+#define C_DOT	letters[20]
--- test/large-font.c
+++ test/large-font.c
@@ -1,13 +1,7 @@
 /*
  * Simple Graphics - Large Font Display
  *
- * Copyright (c) 2014-2015 Charles McManis, all rights reserved.
- *
- * This source code is licensed under a Creative Commons 4.0 
- * International Public license. 
- *
- * See: http://creativecommons.org/licenses/by/4.0/legalcode for
- * details.
+ * Copyright (c) 2014-2017 Charles McManis, all rights reserved.
  *
  * Display all of the characters in the "large"
  * font so that you can verify the bit positions
@@ -17,15 +11,17 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "gfx.h"
+#include "colors.h"
 
-#define SWIDTH 96
-#define SHEIGHT 11*12
+/* character box is 8 x 12 so screen is 128 x 96 (8 rows of 16) */
+#define SWIDTH 8*16
+#define SHEIGHT 12*8
 
 uint8_t screen[SWIDTH * SHEIGHT];
 
 void
-draw_pixel(int x, int y, uint16_t color) {
-	screen[y*SWIDTH + x] = color & 0xff;
+draw_pixel(void *fb, int x, int y, GFX_COLOR color) {
+	screen[y*SWIDTH + x] = color.raw & 0xff;
 }
 
 void print_screen(void) {
@@ -41,22 +37,22 @@ void print_screen(void) {
 int
 main(int argc, char *argv[]) {
 	uint8_t a;
-	char buf[260];
-	char *t;
+	int	line;
+	GFX_CTX *g;
 
-	printf("Testing the graphics code\n");
+	printf("Functional Test: Dump Large Font\n");
 	
-
-	gfx_init(draw_pixel, SWIDTH, SHEIGHT, GFX_FONT_LARGE);
-	gfx_fillScreen((uint16_t) ' ');
-	gfx_setTextColor((uint16_t) '@', (uint16_t) ' ');
-	gfx_setCursor(0, 9);
-	gfx_setTextWrap(1);
-	for (a = 1, t = &buf[0]; a < 128; a++, t++) {
-		*t = (char) a;
+	g = gfx_init(draw_pixel, SWIDTH, SHEIGHT, GFX_FONT_LARGE, (void *) screen);
+	gfx_fill_screen(g, C_BLANK);
+	gfx_set_text_color(g, C_AT, C_BLANK);
+	gfx_set_text_cursor(g, 0, 9);
+	line = -3;
+	for (a = 0; a < 128; a++) {
+		if ((a % 16) == 0) {
+			line += gfx_get_text_height(g);
+			gfx_set_text_cursor(g, 0, line);
+		}
+		gfx_putc(g, a);
 	}
-	*t = 0;
-	gfx_puts(buf);
-	gfx_write(0);
 	print_screen();
 }
